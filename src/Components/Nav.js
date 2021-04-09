4f7342b633e023349f3d6bb411f6864f4226e785
miShelbyT <mi.shelbyrose@gmail.com>
2021-04-09 09:28:47 -0400

    updated nav bar/header

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -10,12 +10,12 @@ function Nav() {
       <Header as='h2' icon textAlign='center' color='yellow'>
         <Link to='/home'>
           <Icon name='gem' color='blue' />
-          <p style={{color: "light-blue"}}>(⬆ click me ⬆)</p>
+          <p style={{color: "light-blue"}}>(⬆ for more info, click me ⬆)</p>
         </Link>
-        <Header.Content>Welcome To The Mintbean-Inspired (Not-A-Puzzle) Game!</Header.Content>
+        <Header.Content>Welcome To The Mintbean-Inspired Board Game!</Header.Content>
       </Header>
       <Button as={Link} to='/play' inverted color='violet' >
-        Click Here To Begin A Game!
+        Click Here To Begin!
       </Button>
     </div>
   )
