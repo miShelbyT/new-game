bd170a9f99afa29514b940aeef1ad0da304fe84e
miShelbyT <mi.shelbyrose@gmail.com>
2021-05-29 15:03:19 -0400

    updated start button to disappear when we go to game board and reappear with gem onClick

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Header, Icon, Button } from 'semantic-ui-react'
 
 
 function Nav() {
 
+  const [renderBtn, setRenderBtn] = useState(true)
+
+  function handleClick() {
+    return setRenderBtn(!renderBtn)
+  }
+
   return (
     <div className="nav">
       <Header as='h2' icon textAlign='center' color='yellow'>
-        <Link to='/home'>
+        <Link to='/home' onClick={handleClick}>
           <Icon name='gem' color='blue' />
           <p style={{color: "light-blue"}}>(⬆ for more info, click me ⬆)</p>
         </Link>
         <Header.Content>Welcome To New/Improved Tic Tac Toe!</Header.Content>
       </Header>
-      <Button as={Link} to='/play' inverted color='violet' >
+      {renderBtn ? <Button as={Link} to='/play' inverted color='violet' onClick={handleClick}>
         Click Here To Begin
-      </Button>
+      </Button> : null}
     </div>
   )
 
